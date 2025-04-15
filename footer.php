20f35db10324ff3c65af6f45d9dcfd2271e8ef29
clement-queste <questeclement@gmail.com>
2025-04-15 21:28:26 +0200

    Ajout des fichiers HTML et suppression des fichiers PHP

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-        <div class="footer-icons">
-          <a href="mailto:questeclement@gmail.com" title="Email"><i class="fas fa-envelope"></i></a>
-          <a href="tel:+33766319831" title="Téléphone"><i class="fas fa-phone"></i></a>
-          <a href="https://www.linkedin.com/in/cl%C3%A9ment-queste-181713338/" title="LinkedIn" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-          <a href="https://www.instagram.com/clemqueste/" title="Instagram" target="_blank"><i class="fab fa-instagram"></i></a>
-        </div>
-        <p>© 2025 Clément Queste. Mentions légales.</p>
-</footer>
